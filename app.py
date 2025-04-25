568e362f6f1fb76d0a3403e786a89b6d1351a933
Lakshyakumar1603 <lakshyakumar139@gmail.com>
2025-04-25 13:50:12 +0530

    uploaded

--- app.py
+++ app.py
@@ -1,13 +1,81 @@
-from flask import Flask
 
-# Create Flask app instance
+
+from flask import Flask , render_template, url_for, request
+import joblib # type: ignore
+
+model = joblib.load('model.lb')
 app = Flask(__name__)
 
-# Define a route
-@app.route('/')
-def hello():
-    return "Hello, World!"
 
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')                    # (/) --> home route 
+def home():
+    return render_template('index.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contactus():
+    return render_template('contact.html')
+
+@app.route('/history')
+def history():
+    # Pass the historical data to the history.html template for rendering
+    return render_template("history.html", historical_data="Hello")
+
+
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method=='POST':
+        brand_name = request.form['brand_name']
+        owner_name=int(request.form['owner'])
+        age_bike=int(request.form['age'])
+        power_bike= int(request.form['power'])
+        kms_driven_bike= int(request.form['kms_driven'])
+        bike_numbers={'TVS': 0,
+                        'Royal Enfield': 1,
+                        'Triumph': 2,
+                        'Yamaha': 3,
+                        'Honda': 4,
+                        'Hero': 5,
+                        'Bajaj': 6,
+                        'Suzuki': 7,
+                        'Benelli': 8,
+                        'KTM': 9,
+                        'Mahindra': 10,
+                        'Kawasaki': 11,
+                        'Ducati': 12,
+                        'Hyosung': 13,
+                        'Harley': 14,
+                        'Jawa': 15,
+                        'BMW': 16,
+                        'Indian': 17,
+                        'Rajdoot': 18,
+                        'LML': 19,
+                        'Yezdi': 20,
+                        'MV': 21,
+                        'Ideal': 22}
+        
+        brand_name_encode=bike_numbers[brand_name]
+        lst=[[owner_name,brand_name_encode,kms_driven_bike,age_bike,power_bike]]  #sequence order
+        pred = model.predict(lst)
+        pred = pred[0]
+        pred = round(pred, 2)
+        user_data = (str(owner_name),str(brand_name),kms_driven_bike,age_bike,power_bike,pred)
+        return render_template("project.html",prediction=str(pred))
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+
+
+
